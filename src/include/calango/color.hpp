cd8d803273c10a7924fd5ea0e90197e74c1e7ebf
DuarteVonFaul <duarteriograndense@gmail.com>
2024-03-24 23:41:40 -0300

    FEAT[Desenvolvendo as cores padrões da Engine]

---
+++ src/include/calango/color.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+ namespace clg
+{
+    typedef float color[3];
+
+
+    color RED    = {0.85, 0.12, 0.0 };
+    color GREEN  = {0.0,  1.0,  0.0 };
+    color BLUE   = {0.0,  0.15, 0.35};
+    color BLACK  = {0.0,  0.0,  0.0 };
+    color WHITE  = {1.0,  1.0,  1.0 };
+    color YELLOW = {1.0,  1.0,  0.0 };
+    color VIOLET = {0.54, 0.17, 0.88};
+    color GRAY   = {0.8,  0.8,  0.8 };
+}
