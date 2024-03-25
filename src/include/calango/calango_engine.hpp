192faebc3b0bcbacbf20cb4d67e6aae41dfd3f53
DuarteVonFaul <duarteriograndense@gmail.com>
2024-03-24 23:12:17 -0300

    FEAT[Inicializando o Cabeçalho do Modulo Engine] Ele tem alguns defines para entender qual plataforma está sendo compilada, e também se é implementação ou integração

---
+++ src/include/calango/calango_engine.hpp
@@ -0,0 +1,28 @@
+#ifndef CALANGO_ENGINE
+#define CALANGO_ENGINE
+
+#include <calango/calango_platform.hpp>
+
+#ifdef CALANGO_PLATFORM_WINDOWS
+#ifdef CALANGO_ENGINE_IMPLEMENTATION
+#define CALANGO_ENGINE_API __declspec(dllexport)
+#else
+#define CALANGO_ENGINE_API __declspec(dllimport)
+#endif //CALANGO_ENGINE_IMPLEMENTATION
+#else
+#define CALANGO_ENGINE_API
+#endif // CALANGO_PLATFORM_WINDOWS
+
+namespace clg
+{
+  struct CALANGO_ENGINE_API Engine final
+  {
+
+    Platform platform;
+    
+    Engine();
+    ~Engine();
+  };
+}
+
+#endif  //CALANGO_ENGINE
