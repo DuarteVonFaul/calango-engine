26079d118efa66a1d2dd629c8df520ef04c520f6
DuarteVonFaul <duarteriograndense@gmail.com>
2024-03-24 23:45:17 -0300

    FIX[Remoção das antigas funções de ScreenBuild da plataforma Linux]

--- src/platform/calango_platform_linux.cpp
+++ src/platform/calango_platform_linux.cpp
@@ -5,7 +5,7 @@
 namespace clg
 {
 
-    void Platform::showMensage(const char* message){
+    void ScreenBuilder::showMensage(const char* message){
         char text[500];
         snprintf(text, sizeof(text), "zenity --warning \
             --title 'Confirm to Proceed' \
@@ -18,52 +18,6 @@ namespace clg
         } else {
         printf("Exit code: %i\n",WEXITSTATUS(pclose(fp)));
         } 
-    }
-
-    Platform::Platform(){
-        /* Aqui eu inicializo o framework glfw */
-        if (!glfwInit())
-            this->showMensage("Erro ao Inicializar GLFW");
-    };
-    GLFWwindow* Platform::createWindow(int width, int height, const char* title){
-        /* Aqui eu construo a janela */
-        GLFWwindow* window;
-        window = glfwCreateWindow(width, height, title, nullptr, nullptr);
-        if (!window)
-        {
-            glfwTerminate();
-            return nullptr;
-        }
-        return window;
-    };
-
-
-    void Platform::destroyWindow(GLFWwindow* window){
-        glfwDestroyWindow(window);
-        glfwTerminate();
-    };
-
-    bool Platform::initOpenGL(GLFWwindow* window){
-        /* Aqui eu construo o contexto OpenGL e linko com a tela*/
-        glfwMakeContextCurrent(window);
-        glEnable(GL_DEPTH_TEST);
-        return true;
-    };
-
-    // Função para atualizar eventos da janela GLFW
-    void Platform::updateWindowEvents(GLFWwindow* window) {
-        glfwPollEvents();
-        glfwSwapBuffers(window);
-    }
-
-    // Função para obter a flag de fechamento da janela GLFW
-    bool Platform::getWindowCloseFlag(GLFWwindow* window) {
-        return glfwWindowShouldClose(window);
-    }
-
-    // Função para limpar a flag de fechamento da janela GLFW
-    void Platform::clearWindowCloseFlag(GLFWwindow* window) {
-        glfwSetWindowShouldClose(window, GLFW_FALSE);
     }
 
 }
